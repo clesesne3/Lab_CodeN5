6eeaf96c570338f37df5a186151c98ae6c668fd1
Chris Lesesne <clesesne3@gmail.com>
2017-10-07 08:52:40 -0700

    problem 5 passed test

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ function sumArray(testArray){ //eslint-disable-line
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -101,6 +101,6 @@ function multiplyArray(testArray){ //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
